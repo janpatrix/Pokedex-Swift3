d1e0b9047d9dc22af22b83e237591693d5fba98b
Patrick Gross <patrick.gross@native-instruments.de>
2016-11-02 13:49:11 +0100

    Added Background Image

---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Patrick Gross on 02/11/2016.
+//  Copyright © 2016 Patrick Gross. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
