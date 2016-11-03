2a544581ffdc53a3e31c65da571adc71ff9f03ef
Patrick Gross <patrick.gross@native-instruments.de>
2016-11-03 13:48:15 +0100

    Added CellViews

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Patrick Gross on 03/11/2016.
+//  Copyright © 2016 Patrick Gross. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
